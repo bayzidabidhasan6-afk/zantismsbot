844abf43073f0cbd7f735244002d8fa4bd67e57b
bayzidabidhasan6-afk <bayzidabidhasan6@gmail.com>
2026-07-18 19:06:18 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 import os
 
-# Telegram Bot Token
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# তোমার Telegram User ID
-ADMIN_ID = 123456789
+ADMIN_ID = 5432685680
 
-# Bot Name
 BOT_NAME = "zANTI OTP BOT"
 
-# Data Files
 USERS_FILE = "users.json"
 SERVICES_FILE = "services.json"
 NUMBERS_FILE = "numbers.json"
