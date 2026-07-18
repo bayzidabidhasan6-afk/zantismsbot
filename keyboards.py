9613f8e6da8bc168656e65cda479ddd07161e6f6
bayzidabidhasan6-afk <bayzidabidhasan6@gmail.com>
2026-07-18 19:09:17 +0600

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,28 @@
+from telegram import ReplyKeyboardMarkup
 
+def user_keyboard():
+    keyboard = [
+        ["📞 Get Number", "🛡 Checker Set"],
+        ["📊 My Stats", "🚦 Live Traffic"],
+        ["✉️ 2FA Set"]
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True
+    )
+
+
+def admin_keyboard():
+    keyboard = [
+        ["➕ Add Number", "➖ Remove Number"],
+        ["🛠 Add Service", "❌ Delete Service"],
+        ["📢 Broadcast", "👥 User List"],
+        ["📊 Statistics", "🚫 Block User"],
+        ["✅ Unblock User", "⚙ Settings"]
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True
+    )
